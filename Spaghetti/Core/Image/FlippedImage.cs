e2823165d3bb0c501f54eca2c98f87faae9e9f35
Juergen Hock <juergen.hock@iosb.fraunhofer.de>
2024-11-25 09:17:51 +0100

    Cleanup

--- Spaghetti/Core/Image/FlippedImage.cs
+++ Spaghetti/Core/Image/FlippedImage.cs
@@ -1,13 +1,12 @@
 using System;
 
-namespace Spaghetti.Core.Image
-{
-  public sealed class FlippedImage<T>(IImage<T> image) : IImage<T>
-  {
-    public IImage<T> Image { get; } = image;
-    public ImageShape Shape => Image.Shape;
+namespace Spaghetti.Core.Image;
 
-    public T this[int x, int y, int z] =>
-      Image[x, Shape.Height - (y + 1), z];
-  }
+public sealed class FlippedImage<T>(IImage<T> image) : IImage<T>
+{
+  public IImage<T> Image { get; } = image;
+  public ImageShape Shape => Image.Shape;
+
+  public T this[int x, int y, int z] =>
+    Image[x, Shape.Height - (y + 1), z];
 }
